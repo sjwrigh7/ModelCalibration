341596939da3ba156b0453d6c5bb887cf493531c
Stephen Wright <swrigh7@clemson.edu>
2024-12-08 11:34:10 -0500

    added additional package requirements and inclusion statements for all extra files

--- src/ModelCalibration.jl
+++ src/ModelCalibration.jl
@@ -16,6 +16,22 @@ using Parameters
 using JLD2
 using Zygote
 using LatexStrings
+using StatsBase
 # Write your package code here.
+
+include("continuous_samplers.jl")
+include("gaussian_process_kernel.jl")
+include("grid_generation.jl")
+include("griddy_posteriors.jl")
+include("initialization.jl")
+include("likelihood.jl")
+include("post_process.jl")
+include("posterior_functions.jl")
+include("precomputation.jl")
+include("prior_dist_functions.jl")
+include("sampling_functions.jl")
+include("stepsize.jl")
+include("structs.jl")
+include("surrogate.jl")
 
 end
